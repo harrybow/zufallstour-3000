2b70d9d951874835b9b5de7b69a11bce60da8c1c
harrybow <genialitaeter@hotmail.de>
2025-08-11 20:05:19 +0200

    Restore header logo and show on login

--- src/components/HeaderLogo.jsx
+++ src/components/HeaderLogo.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import logo from "../assets/header-logo.svg";
 
 export default function HeaderLogo(){
   return (
     <div className="flex flex-col items-center select-none mt-2 mb-6">
-      <img src="/header-logo.svg" alt="Zufallstour 3000 Logo" className="w-full max-w-xl h-auto" />
+      <img src={logo} alt="Zufallstour 3000 Logo" className="w-full max-w-xl h-auto" />
     </div>
   );
 }
