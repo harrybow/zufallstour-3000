2bb07367986e5c5362d5ef831a5935dc80e419e1
harrybow <genialitaeter@hotmail.de>
2025-08-12 23:46:46 +0200

    Add tests for profile route

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import App from './App.jsx'
 import Profile from './Profile.jsx'
 import { I18nProvider } from './i18n.jsx'
 
-const match = window.location.pathname.match(/^\/profile\/(.+)$/);
+// Detect direct profile links (with optional trailing slash)
+const match = window.location.pathname.match(/^\/profile\/([^/]+)\/?$/);
 const Root = match ? <Profile username={decodeURIComponent(match[1])} /> : <App />;
 
 createRoot(document.getElementById('root')).render(
